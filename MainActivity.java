803db453cc56bae851f3a869b5b233807dc541cb
lakshmiharimca <125427962+lakshmiharimca@users.noreply.github.com>
2023-10-13 11:12:06 +0530

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,65 +1,73 @@
-package com.example.sjcet.ui;
+package com.example.sjcet.lifecycle;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.Toast;
+public class MainActivity extends Activity {
 
-
-
-public class MainActivity extends AppCompatActivity {
-
-    EditText etUsername, etPassword;
-    TextView tvUsernameError, tvPasswordError;
+    private static final String TAG = "Lifecycle";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate: MainActivity");
 
-        etUsername = findViewById(R.id.etUsername);
-        etPassword = findViewById(R.id.etPassword);
-        tvUsernameError = findViewById(R.id.tvUsernameError);
-        tvPasswordError = findViewById(R.id.tvPasswordError);
-
-        Button btnSubmit = findViewById(R.id.btnSubmit);
-        btnSubmit.setOnClickListener(new View.OnClickListener() {
+        Button goToSecondActivityBtn = findViewById(R.id.btnGoToSecondActivity);
+        goToSecondActivityBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                validateInput();
+                startActivity(new Intent(MainActivity.this, MainActivity.class));
             }
         });
     }
 
-    private void validateInput() {
-        String username = etUsername.getText().toString().trim();
-        String password = etPassword.getText().toString().trim();
-        boolean isValid = true;
-
-        // Validate username
-        if (username.isEmpty()) {
-            tvUsernameError.setText("Username is required");
-            isValid = false;
-        } else {
-            tvUsernameError.setText("");
-        }
-
-        // Validate password
-        if (password.isEmpty()) {
-            tvPasswordError.setText("Password is required");
-            isValid = false;
-        } else if (password.length() < 6) {
-            tvPasswordError.setText("Password must be at least 6 characters");
-            isValid = false;
-        } else {
-            tvPasswordError.setText("");
-        }
-
-        if (isValid) {
-            // Perform further actions if input is valid
-            // For example, you can submit the data or navigate to another screen
-        }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart: MainActivity");
+        Toast.makeText(this,"ON START",Toast.LENGTH_SHORT).show();
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume: MainActivity");
+        Toast.makeText(this,"ON RESUME",Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause: MainActivity");
+        Toast.makeText(this,"ON PAUSE",Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop: MainActivity");
+        Toast.makeText(this,"ON STOP",Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: MainActivity");
+        Toast.makeText(this,"ON DESTROY",Toast.LENGTH_SHORT).show();
+
+    }
+
+    // Implement other Activity Lifecycle methods as needed
 }

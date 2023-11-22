25f49e60eac48ed1c5813939103af1f558bfa98d
lakshmiharimca <125427962+lakshmiharimca@users.noreply.github.com>
2023-11-22 10:11:48 +0530

    Add files via upload

---
+++ Main2Activity.java
@@ -0,0 +1,22 @@
+package com.example.sjcet.cycle3q2;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra("key");
+
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(message);
+
+    }
+}
---
+++ MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.sjcet.cycle3q2;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button navigateButton = findViewById(R.id.btnNavigate);
+
+        navigateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                //passing data to the next activity
+                intent.putExtra("key", "Hello from MainActivity!");
+
+                startActivity(intent);
+
+
+            }
+        });
+    }
+}
